a0733c50809803db10d092659ead3970cb9da04d
Yi EungJun <eungjun.yi@navercorp.com>
2014-07-09 11:53:45 +0900

    delete-merged-branch.sh: Show branches to be deleted

--- delete-merged-branch.sh
+++ delete-merged-branch.sh
@@ -8,6 +8,10 @@ base=`git merge-base origin/master origin/next`
 branches=`git branch -r --merged $base | grep '^\s*origin/' | sed 's/^\s*origin\///' | grep -Ev '^(master|maint|next|pu|internal)$' | tr '\n' ' '`
 if [ -n "$branches" ]
 then
+    echo Branches to be deleted:
+    echo $branches | sed 's/ /\n/g' | sed 's/^/  /'
     # prevent unnecessary expansion
     eval "git push origin --delete $branches"
+else
+    echo No branch to be deleted
 fi
