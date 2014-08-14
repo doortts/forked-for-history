f6bce756fa23d51260cada2b5f85cbe668c41210
Yi EungJun <eungjun.yi@navercorp.com>
2014-08-14 15:25:23 +0900

    delete-merged-branch.sh: Delete branches merged into internal

--- delete-merged-branch.sh
+++ delete-merged-branch.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 #
-# Delete branches in the origin repository which is merged into master and next.
+# Delete branches in the origin repository which is merged into both of master
+# and next and/or internal.
 
 git fetch origin
 git remote prune origin
 base=`git merge-base origin/master origin/next`
 branches=`git branch -r --merged $base | grep '^\s*origin/' | sed 's/^\s*origin\///' | grep -Ev '^(master|maint|next|pu|internal)$' | tr '\n' ' '`
+branches="$branches `git branch -r --merged origin/internal | grep '^\s*origin/' | sed 's/^\s*origin\///' | grep -Ev '^(master|maint|next|pu|internal)$' | tr '\n' ' '`"
 if [ -n "$branches" ]
 then
     echo Branches to be deleted:
