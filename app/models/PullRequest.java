27e8cf175d0ea4dac3812c2bd3fb77ca8595efb1
Suwon Chae <sw.chae@navercorp.com>
2015-03-23 14:30:26 +0900

    pull-request: prevent disappering reviewers when new commits added
    Currently, completed reviewer list is reset when new commits added
    to the contributor's pull-request branch. This commit will prevent
    that situation with ongoing reviwer feature.
    When a new commit added to the branch which used for pull-request,
    completed reviewers moved to ongoing reviewer list (instead of reset)
    
    TODO: When a completed reviewer moved to ongoing reviewer,
    It is required to send notification.

--- app/models/PullRequest.java
+++ app/models/PullRequest.java
@@ -1052,6 +1052,7 @@ public class PullRequest extends Model implements ResourceConvertible {
     }
 
     public void clearReviewers() {
+        this.ongoingReviewers.addAll(reviewers);
         this.reviewers = new HashSet<>();
         this.update();
     }
