d1b3f8902edb60d8b3abd9bef59db12783574788
Suwon Chae <sw.chae@navercorp.com>
2015-11-12 15:02:50 +0900

    wid: getAuth & issue posting test

--- app/controllers/MigrationApp.java
+++ app/controllers/MigrationApp.java
@@ -24,7 +24,11 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import controllers.annotation.AnonymousCheck;
 import models.*;
 import models.support.SearchCondition;
+import play.libs.F;
+import play.libs.F.Promise;
 import play.libs.Json;
+import play.libs.ws.WS;
+import play.libs.ws.WSResponse;
 import play.mvc.Result;
 import views.html.migration.home;
 
@@ -32,14 +36,18 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 import java.util.stream.Collectors;
 
+import static play.mvc.Http.Context.Implicit.request;
 import static play.mvc.Results.ok;
 
 @AnonymousCheck
 public class MigrationApp {
     @AnonymousCheck(requiresLogin = true, displaysFlashMessage = true)
-    public static Result migration() {
+    public static Promise<Result> migration() {
         Set<Project> targetProjects = new HashSet<>();
         User worker = UserApp.currentUser();
         for (OrganizationUser organizationUser : worker.organizationUsers) {
@@ -52,8 +60,47 @@ public class MigrationApp {
                 targetProjects.add(projectUser.project);
             }
         }
-        return ok(home.render("Migration",
-                targetProjects.stream().sorted((p1, p2) -> p1.owner.compareTo(p2.owner)).collect(Collectors.toList())));
+        String code = request().getQueryString("code");
+        if(code != null ){
+            final String CLIENT_ID = "bcb6c85038c14c0b8ade";
+            final String CLIENT_SECRET = "e7a2b142550afe1d9262d2008afd2db331f4a0b2";
+            final String ACCESS_TOKEN_URL = "https://oss.navercorp.com/login/oauth/access_token";
+
+            Promise<String> resultPromise = WS.url(ACCESS_TOKEN_URL)
+                .setContentType("application/x-www-form-urlencoded")
+                .post("client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=" + code)
+                .map( new F.Function<WSResponse, String>() {
+                    public String apply(WSResponse response) throws Throwable {
+                        play.Logger.debug(response.getBody());
+                        String accessToken = "";
+                        try {
+                            Pattern p = Pattern.compile("access_token=([^&]+)");
+                            Matcher m = p.matcher(response.getBody());
+                            if(m.find() ){
+                                accessToken = m.group(1);
+                            }
+                        } catch (PatternSyntaxException ex) {
+                            play.Logger.error("Couldn't find access_token");
+                        }
+                        return accessToken;
+                    }
+                });
+            return resultPromise.map(new F.Function<String, Result>() {
+                public Result apply(String token) {
+                    return ok(home.render("Migration",
+                            targetProjects.stream().sorted((p1, p2) -> p1.owner.compareTo(p2.owner)).collect(Collectors.toList()), code, token));
+                }
+            });
+        } else {
+            return Promise.promise(new F.Function0<Result>() {
+                @Override
+                public Result apply() throws Throwable {
+                    return ok(home.render("Migration",
+                            targetProjects.stream().sorted((p1, p2) -> p1.owner.compareTo(p2.owner)).collect(Collectors.toList()), code, null));
+                }
+            });
+        }
+
     }
 
     public static Result project(String owner, String projectName){
@@ -70,6 +117,7 @@ public class MigrationApp {
         }
         result.put("members", Json.toJson(members.toArray()));
         result.put("issueCount", Issue.countIssuesBy(project.id, new SearchCondition()));
+        result.put("boardCount", Posting.countPostings(project));
         return ok(result);
     }
 }
--- public/javascripts/service/yobi.Migration.js
+++ public/javascripts/service/yobi.Migration.js
@@ -22,17 +22,69 @@
     angular.module('yobi.migration', [])
         .constant('SERVER_NAME', "http://127.0.0.1:9000")
         .controller('MigrationController', MigrationController)
-        .service('migrationService', migrationService);
+        .service('migrationService', migrationService)
+        .config(['$httpProvider', function($httpProvider) {
+            $httpProvider.defaults.useXDomain = true;
+            delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        }]);
 
-    function MigrationController(migrationService) {
+    function MigrationController($http, migrationService) {
         /* jshint validthis: true */
         var vm = this;
         vm.project = {};
+        vm.botToken = "f69f60b1a6cd2c3320bd3b19eb4052e528148f4f";
         vm.getProject = getProject;
+        vm.redirect = function () {
+            window.location = "https://oss.navercorp.com/login/oauth/authorize?client_id=bcb6c85038c14c0b8ade&scope=user,repo,admin:org";
+        };
+        vm.getToken = getToken;
+        vm.postIssue = postIssue;
 
         activate();
 
+        console.log('vm.code:', vm.code);
+
         ///////////////////////////
+        function getToken(name){
+            var BASE_URL = 'https://oss.navercorp.com/api/v3';
+            console.log("token: " , vm.token);
+            $http({
+                method: 'GET',
+                url: BASE_URL + '/search/repositories?q=+user:sw-chae',
+                "headers": {
+                    "Authorization": "token " + vm.token
+                }
+            }).then(function successCallback(response) {
+                console.log(response);
+                vm.targetProjects = response.data.items;
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        }
+
+        function postIssue(){
+            var BASE_API = 'https://oss.navercorp.com/api/v3';
+            var issue = {
+                title: "새로운 로고당 - " + Date.now(),
+                body: "이슈가 이상합니다",
+                labels: ["중요도 - 심각"],
+                assignee: "sw-chae"
+            };
+
+            $http({
+                method: 'POST',
+                url: BASE_API + '/repos/sw-chae/sample/issues',
+                "headers": {
+                    "Authorization": "token " + vm.botToken
+                },
+                data: JSON.stringify(issue)
+            }).then(function successCallback(response) {
+                console.log(response);
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        }
+
         function getProject(owner, projectName){
             migrationService.getProject(owner, projectName).then(function (data) {
                 vm.project = data;
@@ -42,6 +94,7 @@
         function activate() {
             // ToDo: health check
         }
+
     }
 
     function migrationService($http, SERVER_NAME) {
